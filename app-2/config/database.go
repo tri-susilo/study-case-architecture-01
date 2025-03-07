5f4d0d9bec98ce83b23de9acbe004b838f4c9d70
Tri Susilo <trisusilo.developer@gmail.com>
2025-03-08 01:59:05 +0700

    change dockerfile, docker-compose and the methode to connect database

--- app-2/config/database.go
+++ app-2/config/database.go
@@ -6,17 +6,11 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
 var MasterDB *sql.DB
 
 func ConnectDB() {
-	// Load .env file
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	// Read database credentials from environment variables
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
@@ -24,13 +18,23 @@ func ConnectDB() {
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	// Build DSN
+	// Check if any variable is missing
+	if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbName == "" {
+		log.Fatal("Database environment variables are not set")
+	}
+
+	// Build DSN (Data Source Name)
 	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?parseTime=true"
 
 	// Open database connection
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// Test the connection
+	if err := db.Ping(); err != nil {
+		log.Fatal("Database connection test failed:", err)
 	}
 
 	log.Println("Connected to database Master")
